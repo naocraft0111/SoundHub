2d362367a71aa565b4cb1501ca68f76875df79aa
naocraft0111 <naoki1306@gmail.com>
2023-01-10 01:47:10 +0900

    route編集 #12

--- src/app/Policies/ArticlePolicy.php
+++ src/app/Policies/ArticlePolicy.php
@@ -16,7 +16,7 @@ class ArticlePolicy
      * @param  \App\Models\User  $user
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function viewAny(User $user)
+    public function viewAny(?User $use)
     {
         return true;
     }
@@ -28,7 +28,7 @@ class ArticlePolicy
      * @param  \App\Models\Article  $article
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function view(User $user, Article $article)
+    public function view(?User $user, Article $article)
     {
         return true;
     }
@@ -65,7 +65,7 @@ class ArticlePolicy
      */
     public function delete(User $user, Article $article)
     {
-        return $user->id === $article->user_id; 
+        return $user->id === $article->user_id;
     }
 
     /**
