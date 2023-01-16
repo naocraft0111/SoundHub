10f0c8d9a42ba2a3eeaec95c9f89f9a5d6969bb0
naocraft0111 <naoki1306@gmail.com>
2023-01-17 00:06:59 +0900

    外部キー制約修正 #33

--- src/database/migrations/2022_12_22_183511_create_articles_table.php
+++ src/database/migrations/2022_12_22_183511_create_articles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('title');
             $table->string('body');
             $table->string('attached_file')->nullable();
