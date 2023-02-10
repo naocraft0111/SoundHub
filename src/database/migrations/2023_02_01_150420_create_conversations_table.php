c54544bad702d15822665ff25c5860f074bce303
naocraft0111 <naoki1306@gmail.com>
2023-02-10 18:06:13 +0900

    ON DELETE CASCADE #61

--- src/database/migrations/2023_02_01_150420_create_conversations_table.php
+++ src/database/migrations/2023_02_01_150420_create_conversations_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('sender_id');
             $table->unsignedBigInteger('receiver_id');
-            $table->foreign('sender_id')->references('id')->on('users');
-            $table->foreign('receiver_id')->references('id')->on('users');
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamp('last_time_message')->nullable();
             $table->timestamps();
         });
--- src/database/migrations/2023_02_02_215006_create_messages_table.php
+++ src/database/migrations/2023_02_02_215006_create_messages_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->foreignId('conversation_id')->constrained();
+            $table->foreignId('conversation_id')->constrained()->onDelete('cascade');
             $table->unsignedBigInteger('sender_id');
             $table->unsignedBigInteger('receiver_id');
             $table->foreign('sender_id')->references('id')->on('users');
