0e2029d3708aad564330343deef11eeb0caec519
naocraft0111 <naoki1306@gmail.com>
2023-03-11 16:26:26 +0900

    プロフィール編集、パスワード変更にパンくずリスト設置 #88

--- src/routes/breadcrumbs.php
+++ src/routes/breadcrumbs.php
@@ -3,9 +3,26 @@
 use Diglactic\Breadcrumbs\Breadcrumbs;
 use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
 
-// 投稿記事TOP 第1階層
+// マイページ 第1階層
+Breadcrumbs::for('users.detail', function (BreadcrumbTrail $trail, $user) {
+    $trail->push('マイページ', route('users.detail', $user->name));
+});
+
+// プロフィール編集 第2階層
+Breadcrumbs::for('users.edit', function (BreadcrumbTrail $trail, $user) {
+    $trail->parent('users.detail', $user);
+    $trail->push('プロフィール編集', route('users.edit', $user->name));
+});
+
+// パスワード変更 第3階層
+Breadcrumbs::for('users.password.edit', function (BreadcrumbTrail $trail, $user) {
+    $trail->parent('users.edit', $user);
+    $trail->push('パスワード変更', route('users.password.edit', $user->name));
+});
+
+// ホーム 第1階層
 Breadcrumbs::for('articles.index', function (BreadcrumbTrail $trail) {
-    $trail->push('投稿記事TOP', route('articles.index'));
+    $trail->push('ホーム', route('articles.index'));
 });
 
 // 新規投稿 第2階層
